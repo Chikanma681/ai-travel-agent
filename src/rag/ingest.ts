4471b80c1a38f8883a1935fd0d625bcb97ff3d92
Chikanma Chika-Onuegbu <chikanmaonuegbu@ymail.com>
2025-08-16 15:21:14 -0600

    upsert working

--- src/rag/ingest.ts
+++ src/rag/ingest.ts
@@ -13,13 +13,33 @@ const index = new UpstashIndex({
 export async function indexHotelData() {
     const csvPath = path.join(process.cwd(), 'src/dataset/montreal_reviews.csv')
     const csvData = fs.readFileSync(csvPath, 'utf-8')
-    const records = parse(csvData, {
+    const records:any = parse(csvData, {
       columns: true,
       skip_empty_lines: true,
     })
 
-
-
+    for (const review of records) {
+    try {
+        await index.upsert({
+          id: review.id, 
+          data: review.text, 
+          metadata: {
+            prediction: review.prediction,
+            prediction_agent: review.prediction_agent,
+            annotation: review.annotation,
+            annotation_agent: review.annotation_agent,
+            multi_label: review.multi_label,
+            explanation: review.explanation,
+            status: review.status,
+            event_timestamp: review.event_timestamp,
+            metadata: review.metadata,
+            metrics: review.metrics,
+          },
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    }
 }
 
 indexHotelData()
